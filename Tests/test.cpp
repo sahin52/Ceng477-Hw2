22d3350ca7829f12c17afbd363bd3d4ac48064be
Sahin <sahinkasap52@gmail.com>
2020-12-12 15:17:25 +0300

    unimportant

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,36 +1,16 @@
 #include <iostream>
 #include <stdarg.h>
-int f(int, ... );
+#include "../printer.h"
 
-int f(int, ... ) {
-    return 0;
-}
-
-int g() {
-        f(1,2,3);
-        return 0;
-}
-
-struct s{
-    int a;
-    int b;
-};
-void TEST(){
-    s q = {
-        .a = 2,
-        .b = 1
-    };
-    s w = q;
-    q.a =3;
-    w.b =5;
-    std::cout << q.a<<q.b;
-    std::cout <<w.a<<w.b;
-
-
+void rotateTest(){
+    auto rot = rotate({0.5, -0.5, -2},45,{0.0, 0.0, 1.0});
+    p(rot);
 }
 
 int main(){
-    std::cout<<"-------------------------------START--------------------\n";
-    TEST();
-    std::cout<<"-------------------------------END-----------------------\n";
+    rotateTest();
+    // translate();
+    // scale();
+
+
 }
